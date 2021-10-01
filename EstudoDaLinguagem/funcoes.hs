20e00368a39a92f5540c2b04f0ed824d865774e0
Kvojps <josebazjunior09@gmail.com>
2021-10-01 18:03:26 -0300

    first commit

---
+++ EstudoDaLinguagem/funcoes.hs
@@ -0,0 +1,18 @@
+impar :: Integral a => a -> Bool
+impar n = n `mod` 2 == 1
+
+quadrado :: Num a => a -> a
+quadrado x = x * x
+
+quadradoMais6Mod9 :: Integral a => a -> a
+quadradoMais6Mod9 x = (x + 6) `mod` 9
+
+raizes2Grau :: Floating a => a -> a -> a -> (a,a)
+raizes2Grau a b c = (x1, x2)
+    where
+        x1 = ((-b) + sqDelta) / (2*a)
+        x2 = ((-b) - sqDelta) / (2*a)
+        sqDelta = sqrt delta
+        delta = b^(2 :: Int) - 4 * a * c
+
+
