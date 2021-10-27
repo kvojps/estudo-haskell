dd25d266cad53277914c01d1f769c346df533c21
Kvojps <josebazjunior09@gmail.com>
2021-10-27 08:56:44 -0300

    Adicionando exercicios

---
+++ ExerciciosFaculdade/Sala00/ex04.hs
@@ -0,0 +1,4 @@
+xMaior :: (Ord a, Floating a) => a -> a -> a -> Maybe a
+xMaior a b c = if delta > 0 then Just((-b + sqrt delta) / 2 * a) else Nothing
+                where
+                    delta = b * b - 4 * a * c
