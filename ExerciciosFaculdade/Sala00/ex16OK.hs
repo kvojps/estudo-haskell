620f0965d95a2677eb55198e0e0441b42492af4b
Kvojps <josebazjunior09@gmail.com>
2021-11-03 09:12:35 -0300

    Adicionando exe sala00

--- ExerciciosFaculdade/Sala00/ex16OK.hs
+++ ExerciciosFaculdade/Sala00/ex16OK.hs
@@ -1,3 +1,13 @@
 inserirPosicao :: [Int] -> Int -> Int -> [Int]
 inserirPosicao xs pos elemento =
     (take pos xs) ++ [elemento] ++ (drop pos xs) 
+
+inserirPosicaoXRec :: (Eq t1, Num t1) => [t2] -> t1 -> t2 -> [t2]
+inserirPosicaoXRec xs 0 e = e : xs
+inserirPosicaoXRec (x:xs) n e = x : inserirPosicaoXRec xs (n-1) e 
+
+--inserirPosicaoXRec [1,2,3,4,5] 3 10
+--1 : inserirPosicaoXRec [2,3,4,5] 2 10
+--1 : 2 : inserirPosicaoXRec [3,4,5] 1 10
+--1 : 2 : 3 : inserirPosicaoXRec [4,5] 0 10
+--1 : 2 : 3 : 10 : [4,5]
--- ExerciciosFaculdade/Sala00/ex18OK.hs
+++ ExerciciosFaculdade/Sala00/ex18OK.hs
@@ -1,2 +1,6 @@
-merge :: [a] -> [a] -> [a]
-merge xs ys = xs ++ ys
+mergeSort :: Ord a => [a] -> [a] -> [a]
+mergeSort [] ys = ys
+mergeSort xs [] = xs
+mergeSort (x:xs) (y:ys) 
+    | x <= y = x : mergeSort xs (y :ys)
+    | otherwise = mergeSort (x:xs) ys
