136dfb2a7eb7a8c208bd9248a6ada32beabf44da
Kvojps <josebazjunior09@gmail.com>
2021-11-07 16:26:03 -0300

    ajustando exercicio

--- ExerciciosFaculdade/Recursividade/recursividadeEx8OK.hs
+++ ExerciciosFaculdade/Recursividade/recursividadeEx8OK.hs
@@ -3,12 +3,12 @@ somaLista [] = 0
 somaLista (x:xs) = x + somaLista xs
 
 obterElementos :: (Eq t, Num t) => t -> [a] -> [a]
-obterElementos 0 xs = []
+obterElementos 0 _ = []
 obterElementos _ [] = []
 obterElementos n (x:xs) = x : obterElementos (n-1) xs
 
 obterUltimoElemento :: [p] -> p 
 obterUltimoElemento [x] = x
-obterUltimoElemento (x:xs) = obterUltimoElemento xs 
+obterUltimoElemento (_:xs) = obterUltimoElemento xs 
 
 --Resolvido
