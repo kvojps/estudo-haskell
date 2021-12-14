d18c8af3ece1f2058171630a77f136a6d05e53a2
kvojps <joseferreira.santosjunior@upe.br>
2021-12-14 16:18:33 -0300

    adicionando codeWars

--- CodeWars/simpleMultiplication.hs
+++
@@ -1,2 +0,0 @@
-simpleMultiplication :: Int -> Int
-simpleMultiplication n = if n `mod` 2 == 0 then n * 8 else n * 9
---
+++ CodeWars/sumOfAllMultiples.hs
@@ -0,0 +1,2 @@
+findSum :: Int -> Int
+findSum n = sum([x | x <- [1..n], x `mod` 3 == 0 || x `mod` 5 == 0])
