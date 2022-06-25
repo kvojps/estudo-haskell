fac118f56a3c69e5903f76a01a07c8ed05fd238a
kvojps <joseferreira.santosjunior@upe.br>
2022-06-25 04:00:08 -0300

    refatorando pacote exercicios

--- exercicios/exerciciosEstudoDaLinguagem/exemplos02/ex02.hs
+++
@@ -1,2 +0,0 @@
-posicao :: Int -> [a] -> a
-posicao n xs = head . drop n 
--- exercicios/exerciciosEstudoDaLinguagem/listas/processamentoDeListas/comprimeConverte.hs
+++ exercicios/exerciciosEstudoDaLinguagem/listas/processamentoDeListas/comprimeConverte.hs
@@ -1,5 +1,7 @@
 import Data.List
 
+comprime :: Eq a => [a] -> [[a]]
 comprime xs = group xs
 
+converte :: [Char] -> [Char]
 converte xs = if length xs > 3 then "!" ++ show (length xs) ++ show (head xs) else xs
