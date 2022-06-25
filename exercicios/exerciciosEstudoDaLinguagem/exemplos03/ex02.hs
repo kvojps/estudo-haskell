962d48b5b82ea932cd7517e5946e6efb6180a6e5
kvojps <joseferreira.santosjunior@upe.br>
2022-06-25 03:34:58 -0300

    refatorando pastas de recursividade

--- exercicios/exerciciosEstudoDaLinguagem/exemplos03/ex02.hs
+++
@@ -1,5 +0,0 @@
-data Expr = Lit Int | Add Expr Expr | Sub Expr Expr | Mult Expr Expr | Div Expr Expr
-
---Tentei realizar essa questão de várias formas, mas não tive sucesso. Tentei definir o data
---como Eq para fazer a comparação entre o recebido da função avaliar com o tipo declarado e 
--- assim operar em cima disso.
