85b75c810ee365f9e1abf2b50ccd3b3673387478
kvojps <joseferreira.santosjunior@upe.br>
2022-06-25 04:17:44 -0300

    adicionando boomBangs.hs

--- estudoDaLinguagem/listas/compreensaoLista/boomBangs.hs
+++ estudoDaLinguagem/listas/compreensaoLista/boomBangs.hs
@@ -1,19 +1,3 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 --Uma compreensão que substitua cada número ímpar maior que 10 com BANG! e cada número ímpar menor que 10 com BOOM!.
 --Se o número não for ímpar, fica fora da lista.
 boomBangs :: Integral a => [a] -> [[Char]]
